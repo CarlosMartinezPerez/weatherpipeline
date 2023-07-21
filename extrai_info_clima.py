af8ecf5999265bbb914af0533976815a7fd4d41e
Carlos Martinez Perez <carlos.perez@compasso.com.br>
2023-07-21 13:42:15 -0300

    Atualizando 210723

--- extrai_info_clima.py
+++ extrai_info_clima.py
@@ -5,7 +5,7 @@ from datetime import datetime, timedelta
 
 # intervalo de datas
 data_inicio = datetime.today()
-data_fim = data_inicio + timedelta(days=30)
+data_fim = data_inicio + timedelta(days=6)
 
 # formatando as datas
 data_inicio = data_inicio.strftime('%Y-%m-%d')
@@ -20,11 +20,21 @@ URL = join('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/se
 dados = pd.read_csv(URL)
 print(dados)
 
-file_path = f'/home/carlosperez/Documents/weatherpipeline/semana={data_inicio}/'
+file_path = f'C:/Users/carlos.perez/developer/WeatherPipeline/results/semana={data_inicio}/'
+
+# Verificar se o diretório já existe
+if os.path.exists(file_path):
+    # Remover o diretório e todos os arquivos dentro dele
+    for root, dirs, files in os.walk(file_path, topdown=False):
+        for file in files:
+            os.remove(os.path.join(root, file))
+        for dir in dirs:
+            os.rmdir(os.path.join(root, dir))
+    os.rmdir(file_path)
+
 os.mkdir(file_path)
 
-
-dados.to_csv(file_path + 'dados_brutos.csv')
-dados[['datetime', 'tempmin', 'temp', 'tempmax']].to_csv(file_path + 'temperaturas.csv')
-dados[['datetime', 'description', 'icon']].to_csv(file_path + 'condicoes.csv')
+dados.to_csv(file_path + 'dados_brutos.csv', mode='w')
+dados[['datetime', 'tempmin', 'temp', 'tempmax']].to_csv(file_path + 'temperaturas.csv', mode='w')
+dados[['datetime', 'description', 'icon']].to_csv(file_path + 'condicoes.csv', mode='w')
 
